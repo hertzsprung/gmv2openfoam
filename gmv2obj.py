06eced5ac047997b6bdb65c084ad89c44b397a32
James Shaw <js102@zepler.net>
2015-12-17 11:30:11 +0000

    fix reorientation to deal with arbitrary pols

--- gmv2obj.py
+++ gmv2obj.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
-from numpy import cross
+from numpy import pi
 from collections import Counter
-from itertools import permutations
+from numpy.linalg import norm
 
 vertices = [] # a list of (x, y, z) tuples
 faces = set() # a set of faces, each element being a list of vertex indices
@@ -20,6 +20,7 @@ with open("faces.dat") as faces_file:
         faces.add(tuple([int(f) for f in face_str.split()]))
     print("Found {f} unique faces".format(f=len(faces)))
 
+# find front faces (must have all vertices with y=0)
 front_faces = []
 for f in faces:
     include = True
@@ -47,31 +48,39 @@ print("Vertices per face stats: ", Counter([len(f) for f in front_faces]))
 
 print("Correcting orientation")
 
-def edge_vectors(face):
-    f = list(face)
-    f.append(face[0])
-    return [np.array(vertices[vb-1])-np.array(vertices[va-1]) for va, vb in zip(f, f[1:])]
+def centre(face):
+    return np.sum(np.array([np.array(vertices[v-1]) for v in face]), axis=0)/len(face)
 
-def vertex_ordering_is_sane(face):
-    edges = edge_vectors(face)
-    for ea, eb in zip(edges, edges[1:]):
-        xprod = cross(ea, eb)
-        if not (xprod[0] == 0 and xprod[2] == 0 and xprod[1] > 0):
-            return False
-    return True
+def edge_vector(centre, v):
+    return centre - np.array(vertices[v-1])
 
-oriented_front_faces = []
+def vertex_with_smallest_angle(edge, candidate_edges):
+    min_angle_vertexI = -1
+    min_angle = 2*pi
+    for i, candidate_edge in enumerate(candidate_edges):
+        # angle needs to be from 0 -- 2*PI, see http://math.stackexchange.com/q/878785/89878
+        dot = np.dot(edge, candidate_edge)
+        det = edge[0] * candidate_edge[2] - edge[2] * candidate_edge[0]
+        angle = np.arctan2(det, dot)
+        if angle < min_angle and angle > 0:
+            min_angle = angle
+            min_angle_vertexI = i
+    return min_angle_vertexI, min_angle
 
-for f in front_faces:
-    for candidate_f in permutations(f):
-        if vertex_ordering_is_sane(candidate_f):
-            oriented_front_faces.append(candidate_f)
-            break
-    else:
-        raise RuntimeError("can't find a suitable orientation for face " + '\t'.join([",".join([str(y) for y in vertices[x-1]]) for x in f]))
+def reorient(face):
+    c = centre(face)
+    oriented_face = list(face)
+    for i, v in enumerate(oriented_face):
+        min_angle_vertexI, min_angle = vertex_with_smallest_angle(edge_vector(c, v), [edge_vector(c, v) for v in oriented_face[i+1:]])
+        if min_angle_vertexI > -1:
+            oriented_face[i+1], oriented_face[i+1+min_angle_vertexI] = oriented_face[i+1+min_angle_vertexI], oriented_face[i+1]
+
+    return oriented_face
+
+front_faces = [reorient(f) for f in front_faces]
 
 with open("surface.obj", "w") as fh:
     for v in vertices:
         print("v {x} {y} {z}".format(x=v[0], y=v[1], z=v[2]), file=fh)
-    for f in oriented_front_faces:
+    for f in front_faces:
         print("f " + " ".join([str(v) for v in f]), file=fh)
