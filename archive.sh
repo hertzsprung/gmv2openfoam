b9200f33465e8d13e2ddeb99474e7533e8cba7ca
James Shaw <js102@zepler.net>
2016-01-28 11:57:21 +0000

    underscore not dash in archive name

--- archive.sh
+++ archive.sh
@@ -3,4 +3,4 @@ if [[ $# -eq 0 ]] ; then
     echo 'Usage: ./archive.sh <version>'
     exit 1
 fi
-git archive --prefix gmv2openfoam-$1/ -o ../gmv2openfoam-$1.orig.tar.gz $1
+git archive --prefix gmv2openfoam-$1/ -o ../gmv2openfoam_$1.orig.tar.gz $1
